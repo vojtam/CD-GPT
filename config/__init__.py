23abfe52ea8ad4812d02c8890ba9287ccee96ced
vojtam <macala.vojtech@gmail.com>
2025-08-10 14:14:23 +0200

    remove old dirs

--- config/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2024, Tencent Inc. All rights reserved.
-# Data: 2024/6/20 17:24
-from .utils import configurable
-from .default import CN, _C as cfg, get_config
--- model/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2024, Tencent Inc. All rights reserved.
-# Data: 2024/6/20 16:38
-from .cd_gpt import CDGPT, CDGPTResiduePairPrediction, CDGPTTokenPrediction, CDGPTSequencePrediction
