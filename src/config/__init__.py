1793001651600b0b133822f1fcfd5c97c7fc4c57
vojtam <macala.vojtech@gmail.com>
2025-08-10 14:11:25 +0200

    put everything under src dir

---
+++ src/config/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024, Tencent Inc. All rights reserved.
+# Data: 2024/6/20 17:24
+from .utils import configurable
+from .default import CN, _C as cfg, get_config
