b38927357640bb6e675b9768caa154ab713c39f7
vojtam <macala.vojtech@gmail.com>
2025-08-10 14:22:28 +0200

    add init.py

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2024, Tencent Inc. All rights reserved.
 # Data: 2024/6/20 16:38
-from .cd_gpt import CDGPT, CDGPTResiduePairPrediction, CDGPTTokenPrediction, CDGPTSequencePrediction
+from .cd_gpt import (
+    CDGPT,
+    CDGPTResiduePairPrediction,
+    CDGPTTokenPrediction,
+    CDGPTSequencePrediction,
+)
